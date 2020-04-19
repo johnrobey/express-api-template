a0817e79cf442c0f722a2bfafc76fbb00959e1df
John Robey <john@johnrobey.com>
2020-04-19 22:06:48 +0100

    02 Dev dependencies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-var logger = require('morgan');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var indexRouter = require('./routes/index');
+import logger from 'morgan';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import indexRouter from './routes/index';
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -11,4 +11,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 
-module.exports = app;
+export default app;
